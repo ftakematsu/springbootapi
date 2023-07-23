4d1b87fb777f6015eacd4ea1b8543e4ebc924535
Fabio Matsunaga <ftakematsu@gmail.com>
2023-07-23 00:51:29 -0300

    BCript password generation

--- src/main/java/com/api/rest/Application.java
+++ src/main/java/com/api/rest/Application.java
@@ -30,6 +30,7 @@ public class Application implements WebMvcConfigurer {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println(new BCryptPasswordEncoder().encode("123"));
 		System.out.println("Server running on port 8081");
 	}
 	
